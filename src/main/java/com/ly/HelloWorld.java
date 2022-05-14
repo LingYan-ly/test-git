ed7c7e0fbef06ea7126fb99f30e555513d9e2357
lingyan <981045551@qq.com>
2022-05-14 10:36:56 +0800

    第二个人

--- src/main/java/com/ly/HelloWorld.java
+++ src/main/java/com/ly/HelloWorld.java
@@ -3,5 +3,6 @@ package com.ly;
 public class HelloWorld {
     public static void  main(String[] args){
         System.out.println("helloworld");
+        System.out.println("我是第二个人写的代码");
     }
 }
